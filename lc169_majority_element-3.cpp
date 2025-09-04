175fbec352a857116d84be97f458d1f04f8f99bc
Jayesh K <jayeshkwal2209@gmail.com>
2025-09-04 14:55:07 +0530

    Create lc169_majority_element-3.cpp

---
+++ lc169_majority_element-3.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int freq=0,ans=0;
+        int n=nums.size();
+        for(i=0;i<n;i++){
+            if(freq=0){
+                ans=nums[i];
+            }
+            if(ans == nums[i]){
+                freq++;
+            }
+            else{
+                freq--;
+            }
+
+        }
+        return ans;
+        
+    }
+};
