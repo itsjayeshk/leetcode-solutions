965fe44f839ecfb35acfac37c5dfb143c2fd4a5a
Jayesh K <jayeshkwal2209@gmail.com>
2025-09-05 15:57:58 +0530

    Create lc1365_numbers_smaller_than_current_number.cpp

---
+++ lc1365_numbers_smaller_than_current_number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n);
+        for(int i = 0;i < n;i++){
+            int temp = nums[i];
+            int counter = 0;
+            for(int j = 0;j < n;j++){
+                if(temp > nums[j]){
+                    counter++;
+                }
+                ans[i] = counter;
+
+            }
+        }
+        return ans;
+    }
+};
