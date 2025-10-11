df92ce6ca257d4c67960dc219e356edef92a141d
Jayesh Khandelwal <jayeshkwal2209@gmail.com>
2025-10-11 15:16:37 +0530

    Create lc2527_Xor_beuty_of_array.cpp

---
+++ lc2527_Xor_beuty_of_array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int xorBeauty(vector<int>& nums) {
+        int a = 0;
+        int b = 0;
+        for(int i = 0;i < nums.size();i++){
+            a = a | nums[i];
+            b = b ^ nums[i];
+        }
+        return (a & b);
+        
+    }
+};
