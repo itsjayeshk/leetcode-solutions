fc4d098909b445a75c4d2ea0a3905e08dacb5576
Jayesh Khandelwal <jayeshkwal2209@gmail.com>
2025-12-01 01:15:59 +0530

    Update and rename array_from_permutations.cpp to lc1920_array_from_permutations.cpp

--- lc1920_array_from_permutations.cpp
+++ lc1920_array_from_permutations.cpp
@@ -2,10 +2,12 @@ class Solution {
 public:
     vector<int> buildArray(vector<int>& nums) {
         int n = nums.size();
-        vector<int> ans(n);
-        for (int i = 0; i < n; i++) {
-            ans[i] = nums[nums[i]];
+        vector<int> ans;
+        for(int i = 0;i < n;i++){
+            ans.push_back(nums[nums[i]]);
+
         }
+  
         return ans;
     }
 };
