400cf4396be7a37795ecec7107bcd7ab0a85cb83
Jayesh Khandelwal <jayeshkwal2209@gmail.com>
2025-12-02 00:55:07 +0530

    Create lc168_excel_sheet_column_title.cpp

---
+++ lc168_excel_sheet_column_title.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string convertToTitle(int n) {
+        string ans = "";
+        while(n > 0){
+            n--;                         
+            int rem = n % 26;
+            ans.push_back('A' + rem);
+            n = n/26;
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
