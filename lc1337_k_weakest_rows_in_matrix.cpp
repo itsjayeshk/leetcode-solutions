09360a77ce5313c18b9dea40d86195756ef8cdf1
Jayesh Khandelwal <jayeshkwal2209@gmail.com>
2025-12-13 21:27:34 +0530

    Create lc1337_k_weakest_rows_in_matrix.cpp

---
+++ lc1337_k_weakest_rows_in_matrix.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
+        vector<pair<int,int>> v;
+
+        for(int i = 0;i < mat.size();i++){
+            int soldiers = 0;
+            for(int j = 0;j < mat[i].size();j++){
+                if(mat[i][j] == 1){
+                    soldiers++;
+                }
+                else break;
+            }
+            v.push_back({soldiers,i});
+        }
+
+        sort(v.begin(),v.end());
+
+        vector<int> ans;
+        for(int i = 0;i < k;i++){
+            ans.push_back(v[i].second);
+        }
+        return ans;
+    }
+};
