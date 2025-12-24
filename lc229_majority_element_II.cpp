35fba229f05c6454ad4a98f5b7f473d13767ecef
Jayesh Khandelwal <jayeshkwal2209@gmail.com>
2025-12-24 15:13:47 +0530

    Create lc229_majority_element_II.cpp

---
+++ lc229_majority_element_II.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        unordered_map<int,int> m;
+        vector<int> ans;
+        for(int i = 0;i < nums.size();i++){
+            if(m.count(nums[i])){
+                m[nums[i]]++;
+            }
+            else{
+                m[nums[i]] = 1;
+            }
+        }
+        for(pair<int,int>p : m){
+            if(p.second > nums.size()/3){
+                ans.push_back(p.first);
+            }
+        }
+        return ans;
+        
+    }
+};
