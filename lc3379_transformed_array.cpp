a70e0254d6ff1ae065a9f6b3604eedb472f6333f
Jayesh Khandelwal <jayeshkwal2209@gmail.com>
2026-02-05 10:30:17 +0530

    Create lc3379_transformed_array.cpp

---
+++ lc3379_transformed_array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> constructTransformedArray(vector<int>& nums) {
+       
+        int n = nums.size();
+        vector<int> result(n,0);
+        for(int i = 0;i < nums.size();i++){
+            int dest = (((i + nums[i]) % n) + n) % n;
+            result[i] = nums[dest];
+        }
+        return result;
+        
+    }
+};
