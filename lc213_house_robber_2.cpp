783e918c04d63ca15e9c5583c16461f7d7cca715
Jayesh Khandelwal <jayeshkwal2209@gmail.com>
2026-02-14 01:12:14 +0530

    Create lc213_house_robber_2.cpp

---
+++ lc213_house_robber_2.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int p1 = 0;
+        int p2 = 0;
+        int p3 = 0;
+        int p4 = 0;
+        for(int i = 0;i < nums.size() - 1;i++){
+            int curr = max(p1,p2 + nums[i]);
+            p2 = p1;
+            p1 = curr;
+        }
+        for(int i = 1;i < nums.size();i++){
+            int curr = max(p3,p4 + nums[i]);
+            p4 = p3;
+            p3 = curr;
+        }
+        return max(p1,p3);
+
+        
+        
+    }
+};
