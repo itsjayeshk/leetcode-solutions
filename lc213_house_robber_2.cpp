bf07710e613c1a2954d8500a125c4c0aeee86bb0
Jayesh Khandelwal <jayeshkwal2209@gmail.com>
2026-02-14 01:14:39 +0530

    Update lc213_house_robber_2.cpp

--- lc213_house_robber_2.cpp
+++ lc213_house_robber_2.cpp
@@ -5,6 +5,9 @@ public:
         int p2 = 0;
         int p3 = 0;
         int p4 = 0;
+        if(nums.size() == 1){
+            return nums[0];
+        }
         for(int i = 0;i < nums.size() - 1;i++){
             int curr = max(p1,p2 + nums[i]);
             p2 = p1;
