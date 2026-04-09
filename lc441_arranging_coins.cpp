2dc598aa9c52b2a6126b0a7cde700108ef0c0c1d
Jayesh Khandelwal <jayeshkwal2209@gmail.com>
2026-04-10 02:58:56 +0530

    Create lc441_arranging_coins.cpp

---
+++ lc441_arranging_coins.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        int rows = 1;
+        while(n >= rows){
+            
+            n = n - rows;
+            rows++;
+
+
+        }
+        return rows-1;
+        
+    }
+};
