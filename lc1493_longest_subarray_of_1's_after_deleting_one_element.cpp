6d78174c16353b84ba32541158874e17d18488a3
Jayesh Khandelwal <jayeshkwal2209@gmail.com>
2026-05-04 12:30:53 +0530

    Create lc1493_longest_subarray_of_1's_after_deleting_one_element.cpp

---
+++ lc1493_longest_subarray_of_1's_after_deleting_one_element.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+        int left = 0;
+        int zero = 0;
+        int maxl = 0;
+        for(int i = 0;i < nums.size();i++){
+            if(nums[i] == 0){
+                zero++;
+            }
+            while(zero > 1){
+                if(nums[left] == 0){
+                    zero--;
+                    left++;
+                }
+            }
+            maxl = max(maxl,i - left + 1);
+        }
+        return maxl;
+
+        
+    }
+};
